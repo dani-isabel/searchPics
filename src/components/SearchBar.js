4d649a35ed487251997ad643723937230c04b739
Daniela Isabel Bolivar Garcia <daniela.bolivar@globant.com>
2020-10-16 17:23:15 -0500

    input component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+    state = { term: ''};
+
+    onFormSubmit(e) {
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input 
+                        type="text"
+                        value={this.state.term}
+                        onChange={(e) => {this.setState({term: e.target.value.toUpperCase()})}}/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
