859df6a50066a28338478ce474ecdf0c42f7122b
Daniela Isabel Bolivar Garcia <daniela.bolivar@globant.com>
2020-10-20 14:56:28 -0500

    axios to get images

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import axios from 'axios';
 import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: []};
+
+ async onSearchSubmit(term) {
+  const response = await axios.get('https://api.unsplash.com/search/photos',{
+      params: { query: term},
+      headers: {
+        Authorization: 'Client-ID acRQlkf7NwGN0k83xrOQwr1LjSdCMSMpOhC78RCb4iA'
+      }
+    });
+    this.setState({ images: response.data.results });
+  } 
   render() {
     return (
       <div className="ui container app">
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images;
       </div>
     );
   }
